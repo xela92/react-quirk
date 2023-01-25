0ed915349aa2995011d003dfd1c6f7035d1e07fb
Alex Florenti <alex@aleflo.it>
2023-01-25 09:40:41 +0100

    handle onSuccess and onError, deps

--- src/lib/hooks/useQuirkState.ts
+++ src/lib/hooks/useQuirkState.ts
@@ -2,7 +2,9 @@ import {useCallback, useEffect, useState} from 'react'
 import {Quirk} from '../quirk'
 
 type QuirkStateConfig = {
-  debug?: boolean
+  debug?: boolean,
+    onSuccess?: (value: any) => void,
+    onError?: (error: Error) => void,
 }
 export default function useQuirkState<T>(
   quirk: Quirk<T>,
@@ -14,7 +16,7 @@ export default function useQuirkState<T>(
       : quirk.initialValue,
   )
 
-  const { debug } = config || {}
+  const { debug, onSuccess, onError } = config || {}
   const [firstLoad, setFirstLoad] = useState(true)
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState<Error | null>(null)
@@ -27,31 +29,33 @@ export default function useQuirkState<T>(
       .then((newValue) => {
         debug && console.log('QUIRK##LOADED', newValue)
         setState(newValue)
+        onSuccess && onSuccess(newValue)
         return Promise.resolve(newValue)
       })
       .catch((err) => {
         setError(err)
+        onError && onError(err)
         return Promise.reject(err)
       })
       .finally(() => setLoading(false))
-  }, [])
+  }, [debug])
 
   useEffect(() => {
     if (firstLoad) {
       debug && console.log('QUIRK##FIRST_LOAD')
-      load()
+      load().then(onSuccess).catch(onError)
     } else {
-      debug && console.log('QUIRK##LOAD')
+      debug && console.log('QUIRK##ALREADY_LOADED')
     }
     setFirstLoad(false)
-  }, [load, firstLoad])
+  }, [load, firstLoad, debug])
 
   const onChange = useCallback(async (newValue: T): Promise<T> => {
     debug && console.log('QUIRK##ONCHANGE', newValue)
     const updatedValue = await quirk.set(newValue)
     setState(updatedValue)
     return Promise.resolve(updatedValue)
-  }, [])
+  }, [debug, quirk])
 
   return [state as T, onChange, { reload: load, loading, error }] as const
 }
